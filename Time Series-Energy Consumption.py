a62a138dbba987be3e926a04415de108b8ba9f98
roudgarian <roudgarian@gmail.com>
2018-11-04 19:11:28 +0100

    19-10:04-11-18

--- Time Series-Energy Consumption.py
+++ Time Series-Energy Consumption.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[]:
 
 
 import pandas as pd
@@ -10,7 +10,7 @@ import seaborn as sns
 import numpy as np
 
 
-# In[4]:
+# In[]:
 
 
 data= pd.read_table('household_power_consumption.txt', sep=';', 
@@ -18,13 +18,13 @@ data= pd.read_table('household_power_consumption.txt', sep=';',
                  low_memory=False, index_col='dt')
 
 
-# In[5]:
+# In[]:
 
 
 data=data['2007-01-01':'2010-11-27']
 
 
-# In[6]:
+# In[]:
 
 
 data["Global_active_power"] = data.Global_active_power.convert_objects(convert_numeric=True)
@@ -35,75 +35,75 @@ data["Sub_metering_1"] = data.Sub_metering_1.convert_objects(convert_numeric=Tru
 data["Sub_metering_2"] = data.Sub_metering_2.convert_objects(convert_numeric=True)
 
 
-# In[7]:
+# In[]:
 
 
 data.Global_active_power=(data.Global_active_power*1000)/60
 
 
-# In[8]:
+# In[]:
 
 
 data.Global_reactive_power=(data.Global_reactive_power*1000)/60
 
 
-# In[9]:
+# In[]:
 
 
 data["Sub_metering_4"]= data.Global_active_power - data.Sub_metering_1 - data.Sub_metering_2 - data.Sub_metering_3
 
 
-# In[10]:
+# In[]:
 
 
 data.head()
 
 
-# In[12]:
+# In[]:
 
 
 decimals = pd.Series([2,2,2], index=['Global_active_power','Global_reactive_power', 'Sub_metering_4'])
 data.round(decimals)
 
 
-# In[13]:
+# In[]:
 
 
 data.info()
 
 
-# In[14]:
+# In[]:
 
 
 data.isnull().sum()
 
 
-# In[15]:
+# In[]:
 
 
 data=data.dropna(how='any')
 
 
-# In[16]:
+# In[]:
 
 
 data.Global_active_power.resample('D').mean().plot(title='Daily Global Power', color='gray')
 
 
-# In[17]:
+# In[]:
 
 
 data['Global_active_power'].resample('M').mean().plot(kind='bar',color='blue',subplots = True)
 
 
-# In[18]:
+# In[]:
 
 
 data['Sub_metering_4'].resample('M').mean().plot(kind='bar',color='r',subplots = True)
 #data['Sub_metering_1'].resample('M').mean().plot(kind='bar',color='red',subplots = True)
 
 
-# In[50]:
+# In[]:
 
 
 data_sorted =data.resample('Y').mean()
@@ -111,7 +111,7 @@ we=data_sorted.sort_values(by='Global_active_power', ascending=False, na_positio
 we.head()
 
 
-# In[51]:
+# In[]:
 
 
 data.Global_active_power.resample('Y').mean().plot(color='b', legend=True)
@@ -124,15 +124,7 @@ data.Sub_metering_4.resample('Y').mean().plot(color='black', legend=True)
 plt.show()
 
 
-# In[52]:
-
-
-data_sorted =data.resample('Q').mean()
-we=data_sorted.sort_values(by='Global_active_power', ascending=False, na_position='first')
-we.head()
-
-
-# In[48]:
+# In[]:
 
 
 data.Global_active_power.resample('Q').mean().plot(color='b', legend=True)
@@ -145,15 +137,7 @@ data.Sub_metering_4.resample('Q').mean().plot(color='black', legend=True)
 plt.show()
 
 
-# In[53]:
-
-
-data_sorted =data.resample('M').mean()
-we=data_sorted.sort_values(by='Global_active_power', ascending=False, na_position='first')
-we.head()
-
-
-# In[54]:
+# In[]:
 
 
 data.Global_active_power.resample('M').mean().plot(color='b', legend=True)
@@ -166,15 +150,106 @@ data.Sub_metering_4.resample('M').mean().plot(color='black', legend=True)
 plt.show()
 
 
-# In[37]:
+# In[]:
 
 
 #we.plot(color='blue',legend=True)
 
 
-# In[ ]:
+# In[]:
 
 
 #sns.heatmap(data, xticklabels=data.Global_active_power, yticklabels=data.Sub_metering_4, vmin=-1, vmax=1)
 #plt.show() 
+
+
+# In[]:
+
+
+data_2007=data['2007-01-01':'2007-12-31']
+data_2008=data['2008-01-01':'2008-12-31']
+data_2009=data['2009-01-01':'2009-12-31']
+data_2010=data['2010-01-01':'2010-12-31']
+
+
+# In[]:
+
+
+data_sorted =data_2009.resample('M').mean()
+Max=data_sorted.sort_values(by='Global_active_power', ascending=False, na_position='first')
+Max.head()
+
+
+# In[]:
+
+
+data_sorted =data_2009.resample('M').mean()
+Min=data_sorted.sort_values(by='Global_active_power', ascending=True, na_position='first')
+Min.head()
+
+
+# In[]:
+
+
+data.Sub_metering_1.resample('M').mean().plot(legend=True)
+plt.show()
+
+
+# In[]:
+
+
+week01=data['2009-07-20':'2009-07-26']
+day01=data['2009-07-20':'2009-07-20']
+week02=data['2009-01-05':'2009-01-11']
+day02=data['2009-01-06':'2009-01-06']
+#jan=data['2007-01-01':'2007-01-31']
+#Feb=data['2007-02-01':'2007-02-28']
+
+
+# In[]:
+
+
+week01.Sub_metering_1.plot(legend=True)
+week01.Sub_metering_2.plot(legend=True)
+week01.Sub_metering_3.plot(legend=True)
+week01.Sub_metering_4.plot(legend=True)
+plt.xlabel('20July-27July 2009')
+plt.ylabel('Kw/h')
+plt.show()
+
+# In[]:
+
+
+fig, ax =plt.subplots(1)
+day01.Sub_metering_1.plot(legend=True)
+day01.Sub_metering_2.plot(legend=True)
+day01.Sub_metering_3.plot(legend=True)
+day01.Sub_metering_4.plot(legend=True)
+plt.xlabel('22-July-2009')
+plt.ylabel('Kw/h')
+plt.show()
+
+
+# In[]:
+
+
+week02.Sub_metering_1.plot(legend=True)
+week02.Sub_metering_2.plot(legend=True)
+week02.Sub_metering_3.plot(legend=True)
+week02.Sub_metering_4.plot(legend=True)
+plt.xlabel('18May-24May 2009')
+plt.ylabel('Kw/h')
+plt.show()
+
+# In[]:
+
+
+fig, ax =plt.subplots(1)
+day02.Sub_metering_1.plot(legend=True)
+day02.Sub_metering_2.plot(legend=True)
+day02.Sub_metering_3.plot(legend=True)
+day02.Sub_metering_4.plot(legend=True)
+plt.xlabel('06-Jan-2009')
+plt.ylabel('Kw/h')
+plt.show()
 
